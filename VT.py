b1013298b89ae12262217e3d7b5f8423d8b9355f
Imbark99 <143545949+Imbark99@users.noreply.github.com>
2023-08-29 23:07:08 +0300

    Update VT.py

--- VT.py
+++ VT.py
@@ -8,45 +8,52 @@ def get_virustotal_data(item):
     headers = {
         "x-apikey": API_KEY
     }
-    
-    if len(item.split('.')) == 4:  # Assuming it's an IP
-        response = requests.get(f'https://www.virustotal.com/api/v3/ip_addresses/{item}', headers=headers)
-        if response.status_code == 200:
-            data = response.json()
-            return {
-                "item": item,
-                "score": f"{data['data']['attributes']['last_analysis_stats']['malicious']}/{data['data']['attributes']['last_analysis_stats']['harmless']}",
-                "reputation": data['data']['attributes']['reputation'],
-                "country": data.get('data', {}).get('attributes', {}).get('country', 'N/A'),
-                "domain_name": ", ".join([domain['id'] for domain in data.get('data', {}).get('relationships', {}).get('resolutions', {}).get('data', [])])
-            }
-        else:
-            return {"item": item, "error": response.text}
 
-    return {"item": item, "error": "Unrecognized format"}
+    # Determine if it's an IP, Hash or URL
+    if len(item.split('.')) == 4:  # Assuming it's an IP
+        endpoint = f'https://www.virustotal.com/api/v3/ip_addresses/{item}'
+    elif len(item) == 32 or len(item) == 40 or len(item) == 64:  # Assuming it's a hash (MD5, SHA1, SHA256)
+        endpoint = f'https://www.virustotal.com/api/v3/files/{item}'
+    else:  # Assuming it's a URL
+        endpoint = f'https://www.virustotal.com/api/v3/urls/{item}'
+
+    response = requests.get(endpoint, headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return {
+            "item": item,
+            "type": "IP" if len(item.split('.')) == 4 else ("Hash" if (len(item) in [32, 40, 64]) else "URL"),
+            "score": f"{data['data']['attributes']['last_analysis_stats']['malicious']}/{data['data']['attributes']['last_analysis_stats']['harmless']}",
+            "reputation": data['data']['attributes'].get('reputation', 'N/A'),
+            "details": ", ".join([data['id'] for data in data.get('data', {}).get('relationships', {}).get('resolutions', {}).get('data', [])])
+        }
+    else:
+        return {"item": item, "error": response.text}
 
 def main():
     st.title("VirusTotal Bulk Lookup")
 
     uploaded_file = st.file_uploader("Upload CSV or TXT file", type=['csv', 'txt'])
+    user_input = st.text_area("Paste your hashes/IPs/URLs here (newline separated)")
+    submit_button = st.button("Submit")
 
-    if uploaded_file:
-        if uploaded_file.type == "text/csv":
-            data = pd.read_csv(uploaded_file)
-        else:
-            data = pd.DataFrame({'item': [line.strip().decode('utf-8') for line in uploaded_file.readlines()]})
+    if submit_button:
+        if uploaded_file:
+            if uploaded_file.type == "text/csv":
+                data = pd.read_csv(uploaded_file)
+            else:
+                data = pd.DataFrame({'item': [line.strip().decode('utf-8') for line in uploaded_file.readlines()]})
 
-        results = [get_virustotal_data(item) for item in data['item']]
-        df_results = pd.DataFrame(results)
+            results = [get_virustotal_data(item) for item in data['item']]
+            df_results = pd.DataFrame(results)
 
-        st.table(df_results)
+            st.table(df_results)
 
-        csv = df_results.to_csv(index=False)
-        st.download_button("Download CSV", csv, "output.csv")
+            csv = df_results.to_csv(index=False)
+            st.download_button("Download CSV", csv, "output.csv")
 
-    else:
-        user_input = st.text_area("Paste your hashes/IPs here (newline separated)")
-        if user_input:
+        elif user_input:
             items = user_input.split('\n')
             results = [get_virustotal_data(item.strip()) for item in items]
             df_results = pd.DataFrame(results)
